8aae9880181c7bd29a543d0cc13b3e51257b3e9c
manibharathir <manibharathir@gmail.com>
2026-03-22 17:27:06 +0530

    Conditional

--- src/components/Profile/Logo.jsx
+++ src/components/Profile/Logo.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 export default function Logo() {
+
+    const [isOwner, setIsOwner] = useState(false);
+
+    const handleLogoClick = (e) => {
+        console.log("isOwner",isOwner);
+        if (e.shiftKey) {
+            setIsOwner(true);
+        }
+    };
+
     return (
-        <div className='py-[15px] px-[20px] flex justify-between items-center'>
+        <div className='py-[15px] px-[20px] flex justify-between items-center' onClick={handleLogoClick}>
             <div className="logo_wrap relative h-[40px]">
                 <div className="logo_curve_bg1 absolute top-0 left-[35px]"></div>
                 <div className="logo_curve_bg2 absolute top-0 left-[4px]"></div>
                 <div className="logo absolute top-[0px] left-0 text-3xl">MBR</div>
             </div>
             <div className="">
-                <a href="Mani Bharathi Rajendran.pdf" download class="download-btn py-1 px-3 text-sm rounded-[4px] bg-black hover:bg-[#333] text-white no-underline">Download CV</a>
+                <a href={isOwner ? "Mani Bharathi Rajendran V2.pdf":"Mani Bharathi Rajendran.pdf"} download className="download-btn py-1 px-3 text-sm rounded-[4px] bg-black hover:bg-[#333] text-white no-underline">Download CV</a>
             </div>
         </div>
     )
